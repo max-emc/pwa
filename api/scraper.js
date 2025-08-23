0dd560ca17f962984f0be7ddefa4a925dbad3cd2
max-emc <delacencelleriem@gmail.com>
2025-08-23 23:50:57 +0200

    Ajout du débogage node.js

--- api/scraper.js
+++ api/scraper.js
@@ -1,44 +1,3 @@
-// import chromium from "chrome-aws-lambda";
-// import puppeteer from "puppeteer-core";
-
-// async function scrapeWebsite(url) {
-//   const browser = await puppeteer.launch({
-//     args: chromium.args,
-//     defaultViewport: chromium.defaultViewport,
-//     executablePath: await chromium.executablePath,
-//     headless: chromium.headless,
-//   });
-
-//   try {
-//     const page = await browser.newPage();
-//     await page.goto(url, { waitUntil: "networkidle2" });
-//     const content = await page.content();
-//     return content;
-//   } finally {
-//     await browser.close();
-//   }
-// }
-
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const { url } = req.body;
-
-//     if (!url) {
-//       return res.status(400).json({ error: "Aucune URL fournie" });
-//     }
-
-//     try {
-//       const urlContent = await scrapeWebsite(url);
-//       res.status(200).json({ content: urlContent });
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ error: err.message });
-//     }
-//   } else {
-//     res.status(405).json({ error: "Méthode non autorisée" });
-//   }
-// }
-
 import { chromium } from "playwright";
 
 async function isValidUrl(url) {
@@ -55,7 +14,7 @@ export default async function handler(req, res) {
 
   const { url } = req.body;
   if (!url) return res.status(400).json({ error: "No URL provided" });
-  if (!isValidUrl(url)) return res.status.json({ error: "URL is invalid"})
+  if (!isValidUrl(url)) return res.status(400).json({ error: "URL is invalid" });
 
   let browser;
   try {
@@ -65,8 +24,12 @@ export default async function handler(req, res) {
     const urlContent = await page.content();
     res.status(200).json({ content: urlContent });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
+      console.error("Scraper error:", err);
+      res.status(500).json({
+      error: err.message,
+      stack: err.stack,
+      name: err.name,
+    });
   } finally {
     if (browser) await browser.close();
   }
